79c354b55caf293d900d56c112721ec730d3d06f
Madison <midunitz@ucdavis.edu>
2016-02-08 16:53:31 -0800

    still working on user controller

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
 angular.module('myApp', [])
 
+myApp.controller('MyController', ['$scope', function($scope){
+    $scope.currentYear=Date.today().getYear;
+    $scope.currentMonth=Date.today().getMonth;
+    $scope.currentDate=Date.today().getDay;
+
+
+    $scope.userDOB=function(){
+       var age = $scope.currentYear-$scope.birthYear
+        console.log('year', $scope.currentYear);
+        console.log('month', $scope.currentMonth);
+        console.log('date', $scope.currentDate);
+
+       $compareYear(age);
+    }
+    $scope.compareYear=function(Age){
+        if(Age > 25){
+            //show baby pic
+        }else if(Age <16){
+            //show old man pic
+        }else if(Age<20){
+            //calculate milliseconds to 20, add timer countdown template
+            //show revenant picture
+        }
+        else{
+            compareDate(Age)
+        }
+    }   
+
+    $scope.compareDate=function(){
+
+        //else
+            //create countdown timer
+            //show thirsty pic
+
+    }
+
+
+}])
